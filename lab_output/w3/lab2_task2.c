b2b7f650d4f52118fa78bcde769ea5b1d1be67b1
thaidao <daonamthai@gmail.com>
2024-09-17 16:08:48 -0400

    [feat] modify task 2 for beginner

--- lab_output/w3/lab2_task2.c
+++ lab_output/w3/lab2_task2.c
@@ -3,32 +3,29 @@
 int main() {
     int x, y;
 
-    // Ask user for input
+    // Ask user for input x
     printf("Enter the x-coordinate: ");
     scanf("%d", &x);
 
+	// Ask user for input y
     printf("Enter the y-coordinate: ");
     scanf("%d", &y);
 
     // Determine the quadrant using nested if-else statements
-    if (x == 0 && y == 0) {
-        printf("The point (%d, %d) is at the origin.\n", x, y);
-    } else if (x == 0) {
-        printf("The point (%d, %d) is on the y-axis.\n", x, y);
-    } else if (y == 0) {
-        printf("The point (%d, %d) is on the x-axis.\n", x, y);
+    if (x == 0 || y == 0) {
+        printf("Your point is at the axis.\n");
     } else {
         if (x > 0) {
             if (y > 0) {
-                printf("The point (%d, %d) is in Quadrant I.\n", x, y);
+                printf("The point is in Quadrant I.\n");
             } else {
-                printf("The point (%d, %d) is in Quadrant IV.\n", x, y);
+                printf("The point is in Quadrant IV.\n");
             }
         } else {
             if (y > 0) {
-                printf("The point (%d, %d) is in Quadrant II.\n", x, y);
+                printf("The point is in Quadrant II.\n");
             } else {
-                printf("The point (%d, %d) is in Quadrant III.\n", x, y);
+                printf("The point is in Quadrant III.\n");
             }
         }
     }
