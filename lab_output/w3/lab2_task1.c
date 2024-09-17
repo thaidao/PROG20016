4aab3878489cda140ecd49e11687b0b2f433e8be
thaidao <daonamthai@gmail.com>
2024-09-17 16:00:44 -0400

    [feat] change the variable type of task 1 from double to integer

--- lab_output/w3/lab2_task1.c
+++ lab_output/w3/lab2_task1.c
@@ -2,26 +2,20 @@
 #include <stdlib.h>
 
 int main() {
-    double num1, num2, result;
+    int num1, num2, result;
     char operation;
 
-    printf("Simple Calculator\n");
+    printf("My Calculator\n");
 
     // Get user input
     printf("Enter the first number: ");
-    if (scanf("%lf", &num1) != 1) {
-        printf("Invalid input. Please enter a number.\n");
-        return 1;
-    }
+    scanf("%d", &num1);
 
     printf("Enter the second number: ");
-    if (scanf("%lf", &num2) != 1) {
-        printf("Invalid input. Please enter a number.\n");
-        return 1;
-    }
+    scanf("%d", &num2);
 
     printf("Enter the operation (+, -, *, /): ");
-    scanf(" %c", &operation);  // Note the space before %c to consume any whitespace
+    scanf(" %c", &operation);
 
     // Perform calculation using switch
     switch (operation) {
@@ -43,12 +37,12 @@ int main() {
             }
             break;
         default:
-            printf("Invalid operation. Please use +, -, *, or /.\n");
+            printf("Invalid operation.\n");
             return 1;
     }
 
     // Display the result
-    printf("Result: %.2f %c %.2f = %.2f\n", num1, operation, num2, result);
+    printf("Result: %d %c %d = %d\n", num1, operation, num2, result);
 
     return 0;
 }
